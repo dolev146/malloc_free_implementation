b49a72a0c80814a86e488cab9187ee2be300cb4d
yakovkhodorkovski <yaakov103@gmail.com>
2022-04-26 10:30:59 -0700

    added zip of clion memaloc and fixed free of temp text.

--- main.c
+++ main.c
@@ -5,49 +5,52 @@
 
 int main()
 {
-    int *p, *q, *r, *s;
+    // int *p, *q, *r, *s;
 
-    p = malloc(sizeof(*p) * 16);
-    for (int i = 0; i < 16; ++i)
-    {
-        p[i] = i;
-    }
-    for (int i = 0; i < 16; ++i)
-    {
-        printf("%d ", p[i]);
-    }
-    printf("\n");
+    // p = malloc(sizeof(*p) * 1000);
+    // for (int i = 0; i < 1000; ++i)
+    // {
+    //     p[i] = i;
+    // }
+    // for (int i = 0; i < 1000; ++i)
+    // {
+    //     printf("%d ", p[i]);
+    // }
+    // printf("\n");
 
-    printf("memory location p = %p\n", p);
-    free(p);
-    p = NULL;
+    // printf("memory location p = %p\n", p);
+    // free(p);
+    // p = NULL;
 
-    q = malloc(sizeof(*q) * 4);
-    printf("memory location q = %p\n", q);
+    // q = malloc(sizeof(*q) * 4);
+    // printf("memory location q = %p\n", q);
 
-    r = malloc(sizeof(*r) * 4);
-    printf("memory location r = %p\n", r);
+    // r = malloc(sizeof(*r) * 4);
+    // printf("memory location r = %p\n", r);
 
-    s = malloc(sizeof(*s) * 4);
-    printf("memory location s = %p\n", s);
+    // s = malloc(sizeof(*s) * 4);
+    // printf("memory location s = %p\n", s);
 
-    free(q);
-    q = NULL;
-    free(s);
-    s = NULL;
-    free(r);
-    r = NULL;
-    char* msg = "dolev and yakov";
+    // free(q);
+    // q = NULL;
+    // free(s);
+    // s = NULL;
+    // free(r);
+    // r = NULL;
 
-    for (size_t i = 0; i < 2000; i++)
+
+
+     char* msg = "dolev and yakov";
+    
+    for (size_t i = 0; i < 50; i++)
     {
         push(msg);
-        sleep(5000);
+        usleep(5000);
     }
-    for (size_t i = 0; i < 2000; i++)
+    for (size_t i = 0; i < 50; i++)
     {
         pop();
-        sleep(5000);
+        usleep(5000);
     }
 
     return 0;
